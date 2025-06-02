3b3a075dabe30217e919d67fe911f89596e9b4ab
Dusha_01 <87571857+Dusha01@users.noreply.github.com>
2025-06-02 23:59:48 +1000

    xml-html-py

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, jsonify
+import xml.etree.ElementTree as ET
+import json
+
+app=Flask(__name__)
+
+def xml_to_dict(element):
+    result = {}
+    if element.attrib:
+        result['@attributes'] = element.attrib
+    
+    for child in element:
+        child_data = xml_to_dict(child)
+        
+        if child.tag in result:
+            if isinstance(result[child.tag], list):
+                result[child.tag].append(child_data)
+            else:
+                result[child.tag] = [result[child.tag], child_data]
+        else:
+            result[child.tag] = child_data
+    
+    if element.text and element.text.strip():
+        if result:
+            result['#text'] = element.text.strip()
+        else:
+            result = element.text.strip()
+    
+    return result
+
+def load_schedule_data():
+    try:
+        tree = ET.parse('data.xml')
+        root = tree.getroot()
+
+        xml_dict = xml_to_dict(root)
+
+        json_data = json.dump(xml_dict, indent=2)
+
+        return json_data
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/data')
+def get_data():
+    json_data = load_schedule_data()
+    if json_data:
+        return jsonify(json.loads(json_data))
+    else:
+        return jsonify({"error": "Не удалось загрузить данные"}), 500
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
